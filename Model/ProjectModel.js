023879956b7e889dc91e7f06630049630c472dee
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-11-08 15:29:08 +0100

    modified project model file

--- Model/ProjectModel.js
+++ Model/ProjectModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ProjectSchema = new mongoose.Schema({
     boards: [
         {
+            _id: String,
             name: String,
             columns: [{
                 name: String,
